e4ef9b0e14d30fbea25b0a954ddcae601c0c29b2
Marco Sene <marcoaurelio.sene@gmail.com>
2022-10-23 19:23:14 +0200

    feat: adding logging

--- src/main/java/com/marcosene/productpricer/api/controller/ProductController.java
+++ src/main/java/com/marcosene/productpricer/api/controller/ProductController.java
@@ -6,6 +6,8 @@ import com.marcosene.productpricer.api.response.ProductPriceResponse;
 import com.marcosene.productpricer.model.ProductPrice;
 import com.marcosene.productpricer.service.ProductService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,16 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 @SecurityRequirement(name = "api-scheme")
 public class ProductController {
 
+    private Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     @Autowired
     ProductService productService;
 
     @PostMapping(path = "/price")
     public ResponseEntity<ProductPriceResponse> getProductPrice(@RequestBody ProductPriceRequest request) {
+        logger.debug("getProductPrice() request=" + request.toString());
         ProductPrice productPrice = productService.getProductPrice(
                 request.getCurrentTime(),
                 request.getProductId(),
                 request.getBrandId());
-        ProductPriceResponse productPriceResponse = ProductPriceMapper.INSTANCE.getResponse(productPrice);
-        return ResponseEntity.ok(productPriceResponse);
+
+        ProductPriceResponse response = ProductPriceMapper.INSTANCE.getResponse(productPrice);
+        logger.debug("getProductPrice() response=" + response.toString());
+        return ResponseEntity.ok(response);
     }
 }
